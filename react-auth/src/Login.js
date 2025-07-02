a8094b9ce440b15c41dfb69231bb3b7f1b214e4f
Marco Ramos <ramosmarco95@gmail.com>
2025-07-01 21:48:34 -0600

    frontend updates

--- react-auth/src/Login.js
+++ react-auth/src/Login.js
@@ -1,25 +1,72 @@
 import React from 'react'
 import { Form, Button } from 'react-bootstrap';
+import { useState } from 'react';
+import axios from "axios";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [login, setLogin] = useState(false);
+
+  const handleSubmit = (e) => {
+    // prevent the form from refreshing the whole page
+    e.preventDefault();
+    // set configurations
+    const configuration = {
+      method: "post",
+      url: "https://nodejs-mongodb-auth-app-tzd3.onrender.com/login",
+      data: {
+        email,
+        password,
+      },
+    };
+
+    // make the API call
+    axios(configuration)
+      .then((result) => {
+        setLogin(true);
+      })
+      .catch((error) => {
+        error = new Error();
+      });
+  }
+
     return (
         <>
             <h2>Login</h2>
-      <Form>
+      <Form 
+      onSubmit={(e)=>handleSubmit(e)}>
+         {/* display success message */}
+        {login ? (
+          <p className="text-success">You Are Logged in Successfully</p>
+        ) : (
+          <p className="text-danger">You Are Not Logged in</p>
+        )}
         {/* email */}
         <Form.Group controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+          type="email"
+            name="email"
+            value={email}
+            placeholder="Enter email"
+            onChange={ (e) => setEmail(e.target.value)} />
         </Form.Group>
 
         {/* password */}
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+          type="password"
+            name="password"
+            value={password}
+            placeholder="Password" 
+            onChange={ (e) => setPassword(e.target.value)}/>
         </Form.Group>
 
         {/* submit button */}
-        <Button variant="primary" type="submit">
+        <Button variant="primary" type="submit"
+        onClick={(e)=>handleSubmit(e)}>
           Submit
         </Button>
       </Form>
--- react-auth/src/Register.js
+++ react-auth/src/Register.js
@@ -1,25 +1,69 @@
 import React from 'react'
 import { Form, Button } from 'react-bootstrap';
+import { useState } from 'react';
+import axios from 'axios';
+
 
 export default function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [register, setRegister] = useState(false);
+
+  const handleSubmit = (e) => {
+    // prevent the form from refreshing the whole page
+    e.preventDefault();
+    // set configurations
+    const config = {
+      method: 'POST',
+      url: 'https://nodejs-mongodb-auth-app-tzd3.onrender.com/register',
+      data: {
+        email,
+        password,
+      },
+    };
+    // make the request
+    axios(config)
+    .then((response) => setRegister(true))
+    .catch((error) => {error = new Error()});
+  }
     return (
         <>
             <h2>Register</h2>
-      <Form>
+      <Form
+        onSubmit={(e) => handleSubmit(e)} >
+          {/* display success message */}
+        {register ? (
+          <p className="text-success">You Are Registered Successfully</p>
+        ) : (
+          <p className="text-danger">You Are Not Registered</p>
+        )}
         {/* email */}
         <Form.Group controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+          type="email"
+          name='email'
+          placeholder="Enter email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)} />
         </Form.Group>
 
         {/* password */}
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+           type="password"
+            placeholder="Password"
+            name='password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)} />
         </Form.Group>
 
         {/* submit button */}
-        <Button variant="primary" type="submit">
+        <Button 
+        variant="primary"
+         type="submit"
+         onClick={(e) => handleSubmit(e)}>
           Submit
         </Button>
       </Form>
